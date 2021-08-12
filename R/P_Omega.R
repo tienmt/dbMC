d148fd697e7d18023ab25423b7dab9f5b25c5d1d
The Tien Mai <thetm@imb-0925.uio.no>
2021-08-12 15:06:08 +0200

    similar to cran
    new on cran

--- R/P_Omega.R
+++ R/P_Omega.R
@@ -1,11 +1,12 @@
 
-#' projection onto to observation set
+#' projection onto observation set
 #' 
 #' @description 
 #' This function returns a matrix where the missing entries are replaced by 0 s.
 #' 
-#' @param a a matrix
-#' @param entri missint entries
+#' @param a a matrix.
+#' @param entri missing entries location.
+#' @return Return a matrix whose its missing entries are replaced by 0 s.
 P_Omega = function(a,entri){
   a[entri] = 0
   return(a)
